e8b2f2d5f9e179b5fcba252bf8013a3f7f11d269
Lucas-bedin <lucasbedin04@gmail.com>
2025-04-01 21:10:29 -0300

    Implementada a conexão com o bando de dados

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', 'postgres', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres', 
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established Conexão com o banco de dados realizada com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados', error);
+  }
 
 const app = express();
 app.use(express.json());
